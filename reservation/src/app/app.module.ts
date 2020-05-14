9ba5c5ccf363c794c80ae93d5d0d231f3244c5f1
Jimmy <francis.jamesii@gmail.com>
2020-05-14 10:21:40 -0600

    Add mat toolbar

--- reservation/src/app/app.module.ts
+++ reservation/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatToolbarModule
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
